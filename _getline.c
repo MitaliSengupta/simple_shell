cf572e5faed15bbadc2e3ccdf617e93129f1869a
MitaliSengupta <mitali.s.auger@gmail.com>
2018-03-25 21:31:46 -0700

    new getline

--- _getline.c
+++ _getline.c
@@ -1,43 +1,84 @@
 #include "shell.h"
 
 /**
- * _getline - custom getline function
- * @lineptr: buffer to fill
- * @n: number of bytes to get
- * Return: number of bytes read
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
-ssize_t _getline(char **lineptr, size_t *n)
+ssize_t parsing(char **buffer, int file, int *end_of_file)
 {
-	ssize_t readcount;
-	size_t bytes = 100, maxbytes = 1024, i;
-	char *buffer;
+	int num, count = 0;
+	char buff[BUF_LENGTH];
 
-	if (*n > 1)
-		bytes = *n;
-	if (*lineptr == NULL)
-		buffer = malloc(sizeof(char) * maxbytes);
-	else
-		buffer = *lineptr;
+	if (!buffer)
+		return (-1);
+	*buffer = malloc(sizeof(char));
+	if (*buffer == NULL)
+		return (-1);
+	(*buffer)[0] = '\0';
 
-	do {
-		readcount = read(STDIN_FILENO, buffer, bytes);
-		if (readcount == -1)
-			return (-1);
-		for (i = 0; buffer[i]; i++)
-		{
-			if (buffer[i] == '\n' || buffer[i] == '\t')
-				buffer[i] = '\0';
-		}
-		if (buffer[i] == '\0')
-		{
-			buffer = _realloc(buffer, 0, i);
+	free_buffer(buff, BUF_LENGTH); /* everytime will flush buffer when valgrinding */
+	num = read(file, buff, BUF_LENGTH - 1);
+
+	while (num > 0)
+	{
+		buff[num + 1] = '\0';
+		*buffer = _strnconcat(*buffer, buff, num);
+		count += num;
+
+		if (tty_checker(file) && buff[num] == '\0')
 			break;
+	}
+	if (num == 0)
+		*end_of_file = 1;
+	if (num == -1)
+		return (-1);
+	return (count);
+}
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+ssize_t _getline(char **buffer, char **line, char **remainder, int file, int *end_of_file)
+{
+	char *check;
+	ssize_t checker;
+
+	if (!line || !remainder)
+		return (-1);
+	if (*remainder == NULL)
+	{
+		if (file == STDIN_FILENO)
+			write(STDOUT_FILENO, "$ ", 2);
+		checker = parsing(buffer, file, end_of_file);
+		if (checker <= 0)
+		{
+			if (*buffer != NULL)
+				free(*buffer);
+			return (checker);
 		}
-		i++;
-	} while (1);
-
-	*lineptr = buffer;
-	n = &i;
-	return ((ssize_t) bytes);
-
+		check = _strtok(line, *buffer, ";\n\0", remainder);
+	}
+	else
+	{
+		check = _strtok(line, NULL, ";\n\0", remainder);
+	}
+	if (*remainder == NULL)
+		free(*buffer);
+	if (check == NULL)
+	{
+		free(*buffer);
+		return (-1);
+	}
+	return (_strlen(check) + 1);
 }
